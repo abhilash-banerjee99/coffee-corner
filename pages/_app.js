5d47f36773859560cd9099b9e7903d9ff5771b50
abhilash-banerjee99 <digiabhi1999@gmail.com>
2021-12-08 23:19:54 +0530

    Feat: Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Image from 'next/image';
+import '../styles/globals.css';
+import styles from '../styles/Home.module.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
