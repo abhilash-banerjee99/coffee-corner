8883aef5876fcf09e867a993f97c7b18496ba79f
abhilash-banerjee99 <digiabhi1999@gmail.com>
2021-12-10 23:29:09 +0530

    Feat: Add hero image and fonts

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Banner from '../components/banner';
 import styles from '../styles/Home.module.css';
 
@@ -20,6 +21,14 @@ export default function Home() {
           buttonText="View stores nearby"
           handleOnClick={handleOnBannerBtnClick}
         />
+        <div className={styles.heroImage}>
+          <Image
+            src="/static/hero-image.webp"
+            alt=""
+            width={700}
+            height={400}
+          />
+        </div>
       </main>
     </div>
   );
